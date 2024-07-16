b8fa4965de1e44e78ff01b9d32839807220be32d
iris <snazzsinclair@gmail.com>
2024-07-16 01:48:27 -0500

    "its rust now! "

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    let mut commitmsg = String::from("\"");
+    args.remove(0);
+    for arg in args {
+        commitmsg.push_str(&arg);
+        commitmsg.push_str(" ");
+    }
+    commitmsg.push_str("\"");
+    println!("{}", commitmsg);
+    let out1 = Command::new("git")
+        .arg("stage")
+        .arg(".")
+        .output()
+        .expect("unable to execute");
+    let out1 = String::from_utf8(out1.stdout).unwrap();
+    print!("{}", out1);
+
+    let out2 = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(commitmsg)
+        .output()
+        .expect("unable to execute");
+    let out2 = String::from_utf8(out2.stdout).unwrap();
+    print!("{}", out2);
+    let out3 = Command::new("git")
+        .arg("push")
+        .output()
+        .expect("unable to execute");
+    let out3 = String::from_utf8(out3.stdout).unwrap();
+    print!("{}", out3);
+}

fd2f25a67fde87688efac383eee346336214f39f
iris <snazzsinclair@gmail.com>
2024-07-15 23:19:09 -0500

    this python i sgetting so cursed

--- lzclone.py
+++ lzclone.py
@@ -1,5 +1,7 @@
-from sys import argv
-from os import system
+from sys import argv, platform
+from os import system, getcwd, chdir
+import sys
+from tempfile import gettempdir
 
 args = argv
 args.pop(0)
@@ -9,6 +11,26 @@ def getname(url):
     seperated = url.split('/')
     name = seperated[-1].split('.')[0]
     return name
+tempdir = gettempdir()
 
 system('git clone ' + args[0])
-system('cd ' + getname(args[0]))
+chdir(getname(args[0]))
+dir = getcwd()
+if platform == 'linux' or platform == 'darwin':
+    with open(tempdir + '/cd.sh', 'w') as f:
+        f.write('cd ' + dir)
+    system(tempdir + '/cd.sh')
+    system('rm ' + tempdir + '/cd.sh')
+if platform == 'win32' or platform == 'cygwin':
+    system('(dir 2>$1 *`|echo CMD);&<# rem #>echo PowerShell > ' + tempdir + '\\term.txt')
+    term = open(tempdir + '\\term.txt', 'r').read()
+    if term == 'CMD':
+        with open(tempdir + '\\cd.bat', 'w') as f:
+            f.write('cd ' + dir)
+        system(tempdir + '\\cd.bat')
+        system('rm ' + tempdir + '\\cd.bat')
+    if term == 'PowerShell':
+        with open(tempdir + '\\cd.ps1', 'w') as f:
+            f.write('Push-Location ' + dir)
+        system(tempdir + 'cd.ps1')
+        system('rm ' + tempdir + '\\cd.bat')
